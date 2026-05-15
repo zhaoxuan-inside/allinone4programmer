7c951d2ccaac6800a18f216b07f648d0cc3e7b95
zhaoxuan <zhaoxuanmail@163.com>
2026-05-15 12:19:13 +0800

    [Feat] copy

--- src/components/custom/CopyText.vue
+++ src/components/custom/CopyText.vue
@@ -7,13 +7,30 @@ const modelValue = defineModel<string>('value')
 </script>
 
 <template>
-  <div v-if="modelValue" class="inline-flex items-center gap-0.5em">
-    <n-ellipsis :style="{ 'max-width': maxLength || '12em' }">
+  <div
+    v-if="modelValue"
+    class="inline-flex items-center gap-0.5em"
+  >
+    <!-- <n-ellipsis>：来自Naive UI组件库，用于处理文本溢出。当内容超出max-width时自动显示省略号 -->
+    <!-- :style动态绑定：内联样式对象，max-width优先使用props传入的maxLength，否则为"12em" -->
+    <n-ellipsis
+      :style="{ 'max-width': maxLength || '12em' }"
+    >
       {{ modelValue }}
     </n-ellipsis>
-    <n-tooltip trigger="hover">
-      <template #trigger>
-        <span v-copy="modelValue" class="cursor-pointer">
+    <!-- trigger="hover"：鼠标悬停时显示提示 -->
+    <n-tooltip
+      trigger="hover"
+    >
+      <!-- #trigger：具名插槽，指定触发展示tooltip的元素 -->
+      <template
+        #trigger
+      >
+        <!-- class="cursor-pointer"：鼠标悬浮时变为手形，表示可点击 -->
+        <span
+          v-copy="modelValue"
+          class="cursor-pointer"
+        >
           <icon-park-outline-copy />
         </span>
       </template>
