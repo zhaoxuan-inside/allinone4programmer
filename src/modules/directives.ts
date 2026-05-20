9602c7e217e1aeb39901c95eca039c2d555d0d0f
zhaoxuan <zhaoxuanmail@163.com>
2026-05-20 11:41:12 +0800

    [Feat] layout

--- src/modules/directives.ts
+++ src/modules/directives.ts
@@ -5,8 +5,10 @@ export function install(app: App) {
   // app.use(plugin) 的内部逻辑就是调用 plugin.install(app)，并把 app 作为参数传入；
   // 这样插件内部才能拿到应用实例去做注册。
   Object.values(
-    import.meta.glob<{ install: (app: App) => void }>('@/directives/*.ts', {
-      eager: true,
-    }),
-  ).map(i => app.use(i))
+    import.meta
+      .glob<{ install: (app: App) => void }>
+      ('@/directives/*.ts', {
+        eager: true,
+      }),
+  ).map(module => app.use(module))
 }
